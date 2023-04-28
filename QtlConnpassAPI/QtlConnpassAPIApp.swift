f37b39370cb8b580ce57f28663cddd4367ae07db
kakizaki hayate <hayate.h.0704@gmail.com>
2023-04-28 14:50:14 +0900

    Projectを作成するにあたって作成された

---
+++ QtlConnpassAPI/QtlConnpassAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QtlConnpassAPIApp.swift
+//  QtlConnpassAPI
+//
+//  Created by 柿崎逸 on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct QtlConnpassAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchView()
+        }
+    }
+}

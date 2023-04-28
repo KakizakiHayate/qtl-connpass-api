56329e664694b2e649cdc075f91b9903e2cc48e7
kakizaki hayate <hayate.h.0704@gmail.com>
2023-04-28 14:45:00 +0900

    通信するためのModelを追加

---
+++ QtlConnpassAPI/Model/EventModel.swift
@@ -0,0 +1,27 @@
+//
+//  EventModel.swift
+//  ConnpassShowList
+//
+//  Created by 柿崎逸 on 2023/03/26.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    var events: [Events]
+}
+
+struct Events: Decodable, Identifiable {
+    var id = UUID()
+    var title: String
+    var catchCopy: String
+    var eventURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case catchCopy = "catch"
+        case eventURL = "event_url"
+    }
+
+}
+

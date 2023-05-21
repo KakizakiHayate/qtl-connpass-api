bb0e64deeaf16319cec6807b6f69b37d1ad4fa59
kakizaki hayate <hayate.h.0704@gmail.com>
2023-05-21 23:53:03 +0900

    Constファイルを追加

---
+++ QtlConnpassAPI/AppConst.swift
@@ -0,0 +1,14 @@
+//
+//  AppConst.swift
+//  QtlConnpassAPI
+//
+//  Created by 柿崎逸 on 2023/05/21.
+//
+
+import Foundation
+
+enum AppConst {
+    enum Text {
+        static let empty = ""
+    }
+}
